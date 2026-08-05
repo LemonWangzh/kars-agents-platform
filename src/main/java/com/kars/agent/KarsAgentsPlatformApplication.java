0c785e8d7a320d99d37e2f4daef3518f4f3ab623
wangzhenghao11 <wangzhenghao11@jd.com>
2026-08-05 13:28:30 +0800

    Initial commit

---
+++ src/main/java/com/kars/agent/KarsAgentsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kars.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarsAgentsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarsAgentsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kars/agent/KarsAgentsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kars.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarsAgentsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
